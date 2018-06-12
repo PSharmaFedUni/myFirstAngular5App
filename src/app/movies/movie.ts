df3114a4368401d24586ac285b8fab424277d913
PraneetSharma <praneets@gmail.com>
2018-06-12 12:32:08 +1000

    Commented Price and rating as data not coming through webapi call

--- src/app/movies/movie.ts
+++ src/app/movies/movie.ts
@@ -3,7 +3,7 @@ export interface IMovie{
     Title: string;
     Year: string;
     Type: string;
-    Price: string;
-    Rating: string;
+    // Price: string;
+    // Rating: string;
     Poster: string;
 }
